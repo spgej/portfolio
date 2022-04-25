0aa58d75112c7a7548d3ed9622deb7737e1c2251
Samuel <spage930@gmail.com>
2022-04-25 14:38:10 -0400

    Add Fourth Page Page and logo

---
+++ pages/FourthPage/Home.js
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+    return (
+        <div>
+            <header className="App-header">
+                <img src="/images/logo-border.png" className="App-logo" alt="logo-border" />
+                <img src="/images/logo.png" className="App-logo-2" alt="logo" />
+            </header>
+        </div>
+    )
+}
