41914eda896f91be9b06328b414e5e01d95b5f4e
Samuel <spage930@gmail.com>
2022-04-28 20:37:20 -0400

    adjust webstack component

--- components/Webstack.js
+++ components/Webstack.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Box } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/image'
 
-const Webstack = (props) => {
+const Webstack = () => {
   return (
     <Box className="stack-box" bg="background" h="80vh" color="white">
-        {props.content}
+        <Image></Image>
     </Box>
   )
 }
