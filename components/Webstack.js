b321dbee345eb504bdb8a987e5bf4d0c8ae7453b
Samuel <spage930@gmail.com>
2022-05-02 16:24:53 -0400

    mess up webstack component

--- components/Webstack.js
+++ components/Webstack.js
@@ -4,21 +4,39 @@ import { FaStore } from "react-icons/fa";
 
 const Webstack = () => {
   return (
-    <Box className="stack-box" bg="background" h="80vh" color="white">
-        <Box style={{
-          textAlign: "center",
-          fontFamily: "Nexa Light",
-          textTransform: "lowercase",
-
-        }}>
-        <h1 style={{fontSize:"2rem"}}>Front-End</h1>
+    <Box className="container-box" style={{width: "100%"}}>
+    <Box className="stack-box" bg="background" h="80vh" color="white" style={{maxWidth: "900px", margin:"auto"}}>
+        <Box className="stack-box-content">
+        <h1 style={{fontSize:"1.5rem"}}>Front-End</h1>
         <FaStore className="icon" size="120px" style={{
           margin: "auto",
-          color: "#15181D"
+          color: "#15181D",
+          margin: "20px auto"
 
         }} />
-        <p>HTML, CSS, JavaScript, React, Chakra-UI, Next.js</p>
+        <p>HTML, CSS, JavaScript, React, Chakra-ui, Next.js</p>
         </Box>
+        <Box className="stack-box-content">
+        <h1 style={{fontSize:"1.5rem"}}>Back-End</h1>
+        <FaStore className="icon" size="120px" style={{
+          margin: "auto",
+          color: "#15181D",
+          margin: "20px auto"
+
+        }} />
+        <p>Node.js, MongoDB, SQL</p>
+        </Box>
+        <Box className="stack-box-content">
+        <h1 style={{fontSize:"1.5rem"}}>Workspace</h1>
+        <FaStore className="icon" size="120px" style={{
+          margin: "auto",
+          color: "#15181D",
+          margin: "25px auto"
+
+        }} />
+        <p>MacOS, Windows, VSCode, Git, Terminal</p>
+        </Box>
+    </Box>
     </Box>
   )
 }
